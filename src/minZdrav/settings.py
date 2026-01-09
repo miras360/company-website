c2413c64b8e46e1f175d2a274be9b6536eba2589
s0mebodhi <62179798+s0mebodhi@users.noreply.github.com>
2026-01-09 15:29:11 +0500

    Add adminsortable2 to INSTALLED_APPS
    Included 'adminsortable2' in the INSTALLED_APPS list to enable sortable admin functionality in the Django project.

--- src/minZdrav/settings.py
+++ src/minZdrav/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'modeltranslation',
     'django_recaptcha',  # Убедитесь, что 'captcha' здесь
+    'adminsortable2',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
